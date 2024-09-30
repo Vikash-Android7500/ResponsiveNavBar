1587ec92f930c88f2f5f530af1a24c8a4e39c92f
Vikash-Android7500 <Vikash75.jm@gmail.com>
2024-09-30 19:20:30 +0530

    new code

--- logic.js
+++ logic.js
@@ -1,7 +1,16 @@
 const NavMenu = document.querySelector(".navMenu");
-const hembergun = document.querySelector(".fa-bars");
+const hembergun = document.querySelector(".hambergun");
+const NavLink = document.querySelectorAll(".navLink");
 
-hembergun.addEventListener('click', () => {
-     hembergun.classList.toggle('fa-xmark');
-     NavMenu.classList.toggle('active');
+hembergun.addEventListener("click", () => {
+  hembergun.classList.toggle("active");
+  NavMenu.classList.toggle("active");
+});
+
+// Menu click hide
+NavLink.forEach(element => {
+  element.addEventListener("click", () => {
+      hembergun.classList.remove("active");
+      NavMenu.classList.remove("active");
+  })
 });
